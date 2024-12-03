37cf31ed56c7d7c6c9ecf7e3e62fdd77224a7de3
Quinn Wilson <Lunky@midworld.org>
2024-12-03 20:12:49 +0000

    add day4

---
+++ src/Day4.hs
@@ -0,0 +1,16 @@
+module Day4
+    ( 
+    day4
+   ,day4b
+   ,_input
+    )
+    where
+    
+day4 :: String -> Int 
+day4 _input = 0 
+
+day4b :: String -> Int
+day4b _input = 0
+
+_input :: String
+_input=""
